edf838fabbc006583e43bc7937a346c4e3a325d6
Tim Eitel <timeitel@outlook.com>
2022-10-11 18:11:25 +0800

    feat: treesitter args

--- nvim/plugin/nvim-treesitter.rc.lua
+++ nvim/plugin/nvim-treesitter.rc.lua
@@ -26,23 +26,20 @@ ts.setup({
     textobjects = {
         select = {
             enable = true,
-            -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
             keymaps = {
                 ["af"] = "@function.outer",
                 ["if"] = "@function.inner",
                 ["ac"] = "@class.outer",
                 ["ic"] = "@class.inner",
+                ["aa"] = "@parameter.outer",
+                ["ia"] = "@parameter.inner",
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding xor succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
             include_surrounding_whitespace = true,
         },
         move = {
             enable = true,
-            set_jumps = false, -- whether to set jumps in the jumplist
+            set_jumps = false,
             goto_next_start = {
                 ["<C-]>"] = "@function.outer",
             },
