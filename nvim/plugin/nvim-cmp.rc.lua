ed90be54210dfdc002158a9c7f3a8050aab25863
Tim Eitel <timeitel@outlook.com>
2022-10-15 17:05:56 +0800

    feat: closing cmp & fallback

--- nvim/plugin/nvim-cmp.rc.lua
+++ nvim/plugin/nvim-cmp.rc.lua
@@ -28,7 +28,13 @@ local mapping = cmp.mapping.preset.insert({
         end
     end, { "i", "c" }),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-h>"] = cmp.mapping.close(),
+    ["<C-h>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.close()
+        else
+            fallback()
+        end
+    end, { "i", "c" }),
     ["<C-l>"] = cmp.mapping(function()
         if cmp.visible() then
             cmp.confirm({ select = true })
