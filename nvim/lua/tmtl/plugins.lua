7165cefdd246b7260d7215984fbb68e87549acff
Tim Eitel <timeitel@outlook.com>
2022-10-17 18:46:37 +0800

    fix!: treesitter breaking changes, pin to previous commit

--- nvim/lua/tmtl/plugins.lua
+++ nvim/lua/tmtl/plugins.lua
@@ -28,6 +28,7 @@ return require("packer").startup(function(use)
 
     use({
         "nvim-treesitter/nvim-treesitter",
+        commit = "4cccb6f494eb255b32a290d37c35ca12584c74d0", -- broken TS highlights
         run = function()
             require("nvim-treesitter.install").update({ with_sync = true })
         end,
