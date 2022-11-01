02dfe2dc616844626f4ad5a88681e1f332e14329
Tim Eitel <timeitel@outlook.com>
2022-11-01 22:59:56 +0800

    rf: plugins

--- nvim/lua/tmtl/plugins.lua
+++ nvim/lua/tmtl/plugins.lua
@@ -1,73 +1,74 @@
 return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
+  use("wbthomason/packer.nvim")
 
-    -- Telescope
-    use({
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    })
-    use("nvim-telescope/telescope-file-browser.nvim")
-    use("nvim-telescope/telescope-ui-select.nvim")
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  use("nvim-telescope/telescope-file-browser.nvim")
+  use("nvim-telescope/telescope-ui-select.nvim")
 
-    -- LSP & completion
-    use("neovim/nvim-lspconfig")
-    use("L3MON4D3/LuaSnip")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-nvim-lua")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-nvim-lsp-document-symbol")
-    use("hrsh7th/cmp-cmdline")
-    use("saadparwaiz1/cmp_luasnip")
-    use("tamago324/cmp-zsh")
-    use("onsails/lspkind-nvim")
-    use("simrat39/rust-tools.nvim")
-    use("jose-elias-alvarez/null-ls.nvim")
+  -- LSP
+  use("neovim/nvim-lspconfig")
+  use("jose-elias-alvarez/null-ls.nvim")
 
-    -- Treesitter
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        commit = "4cccb6f494eb255b32a290d37c35ca12584c74d0", -- breaking changes to highlight tokens
-        run = function()
-            require("nvim-treesitter.install").update({ with_sync = true })
-        end,
-    })
-    use({ "nvim-treesitter/nvim-treesitter-context" })
-    use({ "nvim-treesitter/nvim-treesitter-textobjects" })
+  -- Completion
+  use("L3MON4D3/LuaSnip")
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-nvim-lua")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-nvim-lsp-document-symbol")
+  use("hrsh7th/cmp-cmdline")
+  use("saadparwaiz1/cmp_luasnip")
+  use("tamago324/cmp-zsh")
+  use("onsails/lspkind-nvim")
 
-    -- Git
-    use({ "lewis6991/gitsigns.nvim" })
-    use({ "TimUntersberger/neogit", requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" } })
-    use({ "bobrown101/git-blame.nvim" })
+  -- Treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    commit = "4cccb6f494eb255b32a290d37c35ca12584c74d0", -- breaking changes to highlight tokens
+    run = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+  })
+  use({ "nvim-treesitter/nvim-treesitter-context" })
+  use({ "nvim-treesitter/nvim-treesitter-textobjects" })
 
-    -- 10000x developer
-    use("bkad/CamelCaseMotion")
-    use("inkarkat/vim-ReplaceWithRegister")
-    use("michaeljsmith/vim-indent-object")
-    use("tpope/vim-commentary")
-    use({
-        "kylechui/nvim-surround",
-        tag = "*",
-        config = function()
-            require("nvim-surround").setup()
-        end,
-    })
-    use("ThePrimeagen/harpoon")
-    use("windwp/nvim-autopairs")
-    use("windwp/nvim-ts-autotag")
-    use("karb94/neoscroll.nvim")
-    use("akinsho/toggleterm.nvim")
-    use("ggandor/leap.nvim")
-    use({
-        "jedrzejboczar/possession.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-    })
+  -- Git
+  use({ "lewis6991/gitsigns.nvim" })
+  use({ "TimUntersberger/neogit", requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" } })
+  use({ "bobrown101/git-blame.nvim" })
 
-    -- Styling
-    use("monsonjeremy/onedark.nvim")
-    use("kyazdani42/nvim-web-devicons")
-    use("nvim-lualine/lualine.nvim")
-    use("j-hui/fidget.nvim")
+  -- 10000x developer
+  use("bkad/CamelCaseMotion")
+  use("inkarkat/vim-ReplaceWithRegister")
+  use("michaeljsmith/vim-indent-object")
+  use("tpope/vim-commentary")
+  use({
+    "kylechui/nvim-surround",
+    tag = "*",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  })
+  use("ThePrimeagen/harpoon")
+  use("windwp/nvim-autopairs")
+  use("windwp/nvim-ts-autotag")
+  use("karb94/neoscroll.nvim")
+  use("akinsho/toggleterm.nvim")
+  use("ggandor/leap.nvim")
+  use({
+    "jedrzejboczar/possession.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+  })
+
+  -- Styling
+  use("monsonjeremy/onedark.nvim")
+  use("kyazdani42/nvim-web-devicons")
+  use("nvim-lualine/lualine.nvim")
+  use("j-hui/fidget.nvim")
 end)
