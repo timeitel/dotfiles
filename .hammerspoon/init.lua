53f45c3d4a54fbaeeb59c4e06c57211997425c3c
Tim Eitel <timeitel@outlook.com>
2022-12-02 07:42:23 +0800

    feat: hammerspoon app launcher

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,6 +4,12 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "l", function()
 end)
 --- end love you
 
+--- start test
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "t", function()
+  print(hs.application.frontmostApplication())
+end)
+--- end test
+
 --- start quick open applications
 local function open_app(name)
   return function()
@@ -14,8 +20,18 @@ local function open_app(name)
   end
 end
 
-hs.hotkey.bind({ "ctrl", "cmd" }, "C", open_app("Visual Studio Code"))
-hs.hotkey.bind({ "ctrl", "cmd" }, "G", open_app("Google Chrome"))
-hs.hotkey.bind({ "ctrl", "cmd" }, "T", open_app("iTerm"))
-hs.hotkey.bind({ "ctrl", "cmd" }, "S", open_app("Slack"))
+local open_app_modifiers = { "ctrl", "cmd" }
+
+hs.hotkey.bind(open_app_modifiers, "h", open_app("Brave Browser"))
+hs.hotkey.bind(open_app_modifiers, "j", open_app("Spotify"))
+hs.hotkey.bind(open_app_modifiers, "k", open_app("Rider"))
+hs.hotkey.bind(open_app_modifiers, "l", open_app("kitty"))
+hs.hotkey.bind(open_app_modifiers, "n", open_app("Slack"))
+hs.hotkey.bind(open_app_modifiers, "y", open_app("Obsidian"))
+hs.hotkey.bind(open_app_modifiers, ";", open_app("Messenger"))
 --- end quick open applications
+
+-- TODO: create workspace -> terminal left, browser right half
+-- TODO: window management
+-- TODO: window scrolling and browser forward back H, L
+-- TODO: browser detach tab to window
