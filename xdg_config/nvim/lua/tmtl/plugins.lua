bf6e9407d60b9a874f2ca97e4f13d1c2bc67a12b
Tim Eitel <timeitel@outlook.com>
2023-01-10 17:50:28 +0800

    feat: packer fresh install fn

--- xdg_config/nvim/lua/tmtl/plugins.lua
+++ xdg_config/nvim/lua/tmtl/plugins.lua
@@ -1,3 +1,17 @@
+-- When installing on new machine
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
 
@@ -74,4 +88,10 @@ return require("packer").startup(function(use)
   use("j-hui/fidget.nvim")
   use("folke/tokyonight.nvim")
   use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+
+  -- Automatically set up configuration after cloning packer.nvim
+  -- Leave at the end after all plugins
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
