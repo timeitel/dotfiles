f085b909672dc4dfd1d3dea351a5c1abe7a43504
Tim Eitel <timeitel@outlook.com>
2023-02-02 08:27:50 +0800

    feat: update hs maps

--- home/.hammerspoon/tmtl/apps.lua
+++ home/.hammerspoon/tmtl/apps.lua
@@ -14,8 +14,7 @@ end
 local modifiers = { "ctrl", "cmd" }
 hs.hotkey.bind(modifiers, "h", open_app("Brave Browser"))
 hs.hotkey.bind(modifiers, "j", open_app("Spotify"))
-hs.hotkey.bind(modifiers, "k", open_app("Rider"))
+hs.hotkey.bind(modifiers, "k", open_app("Obsidian"))
 hs.hotkey.bind(modifiers, "l", open_app("kitty"))
 hs.hotkey.bind(modifiers, "n", open_app("Slack"))
-hs.hotkey.bind(modifiers, "y", open_app("Obsidian"))
 hs.hotkey.bind(modifiers, ";", open_app("Messenger"))
