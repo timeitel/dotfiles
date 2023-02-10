90e8492989cf852bc3fc89851295fce4f461189a
Tim Eitel <timeitel@outlook.com>
2023-02-10 08:52:01 +0800

    rf: sort options

--- xdg_config/nvim/lua/tmtl/options.lua
+++ xdg_config/nvim/lua/tmtl/options.lua
@@ -1,59 +1,57 @@
 local o = vim.opt
 
--- Ignore compiled files
-o.wildignore = "__pycache__"
-o.wildignore:append({ "*.o", "*~", "*.pyc", "*pycache*" })
-o.wildignore:append("Cargo.lock")
-
+o.autoindent = true
 o.autoread = true
+o.background = "dark"
 o.backspace = { "eol", "start", "indent" }
+o.belloff = "all"
+o.breakindent = true
+o.cindent = true
 o.cmdheight = 1
 o.completeopt = { "menu", "menuone", "noinsert" }
+o.cursorline = true
+o.diffopt = { "internal", "filler", "closeoff", "hiddenoff", "algorithm:minimal", "vertical" }
+o.expandtab = true
+o.fillchars = { eob = "~", diff = "╱" }
+o.foldenable = false
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldlevel = 0
+o.foldmethod = "expr"
+o.foldmethod = "marker"
 o.history = 500
 o.hlsearch = false
 o.ignorecase = true
 o.incsearch = true
+o.joinspaces = false
+o.laststatus = 3
 o.lazyredraw = true
+o.modelines = 1
 o.number = true
 o.relativenumber = true
+o.scrollbind = false
 o.scrolloff = 10
+o.sessionoptions = o.sessionoptions - "buffers"
+o.shiftwidth = 2
+o.shortmess:append("c")
+o.showbreak = string.rep(" ", 3) -- smart line wrapping
 o.showcmd = true
 o.showmatch = true
 o.showmode = false
+o.signcolumn = "yes:1"
 o.smartcase = true
-o.splitbelow = true
-o.splitright = true
-o.swapfile = false
-o.whichwrap:append("<,>,h,l")
-o.shortmess:append("c")
-
--- Tabs
-o.autoindent = true
-o.expandtab = true
-o.tabstop = 2
-o.shiftwidth = 2
-o.cindent = true
-o.wrap = true
 o.smartindent = true
-
-o.breakindent = true
-o.showbreak = string.rep(" ", 3) -- smart line wrapping
-o.linebreak = true
-
-o.foldmethod = "marker"
-o.foldlevel = 0
-o.modelines = 1
-
-o.belloff = "all"
-
-o.joinspaces = false
-o.fillchars = { eob = "~", diff = "╱" }
-o.diffopt = { "internal", "filler", "closeoff", "hiddenoff", "algorithm:minimal", "vertical" }
+o.splitbelow = true
+o.swapfile = false
+o.tabstop = 2
+o.termguicolors = true
 o.undofile = true
+o.whichwrap:append("<,>,h,l")
+o.wildignore = "__pycache__"
+o.wildignore:append("Cargo.lock")
+o.wildignore:append({ "*.o", "*~", "*.pyc", "*pycache*" })
+o.wrap = true
 
 local group = vim.api.nvim_create_augroup("Options", { clear = true })
-
-o.cursorline = true -- Highlight the current line
 
 local set_cursorline = function(event, value, pattern)
   vim.api.nvim_create_autocmd(event, {
@@ -86,21 +84,9 @@ vim.api.nvim_create_autocmd("VimResized", {
   group = group,
 })
 
-o.laststatus = 3
-o.background = "dark"
-o.termguicolors = true
-o.signcolumn = "yes:1"
-
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
-o.foldenable = false
-
 vim.api.nvim_command([[
 au FileType * set fo=cqrnj
     ]])
-
-o.scrollbind = false
-o.sessionoptions = o.sessionoptions - "buffers"
 
 -- pipe tsc errors into qf list
 -- TODO: use local config
