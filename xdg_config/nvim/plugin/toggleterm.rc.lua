a71adec3ad716dcc68fbe6b8221bd0f56655aed6
Tim Eitel <timeitel@outlook.com>
2023-02-14 23:04:17 +0800

    rf: toggleterm pcall

--- xdg_config/nvim/plugin/toggleterm.rc.lua
+++ xdg_config/nvim/plugin/toggleterm.rc.lua
@@ -1,4 +1,10 @@
-require("toggleterm").setup({
+local ok, toggleterm = pcall(require, "toggleterm")
+if not ok then
+  return
+end
+
+-- prefix terminal number befor toggle open for multiple terminals
+toggleterm.setup({
   open_mapping = [[<C-;>]],
   direction = "float",
   float_opts = {
