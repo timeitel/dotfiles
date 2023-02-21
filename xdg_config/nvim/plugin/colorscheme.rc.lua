cf883f78fb5616977fc8dfc8872c6a921bf88f32
Tim Eitel <timeitel@outlook.com>
2023-02-21 21:58:13 +0800

    feat: diff colors

--- xdg_config/nvim/plugin/colorscheme.rc.lua
+++ xdg_config/nvim/plugin/colorscheme.rc.lua
@@ -11,7 +11,6 @@ tokyo_night.setup({
 vim.cmd([[colorscheme tokyonight-moon]])
 
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#363c58" })
--- vim.api.nvim_set_hl(0, "DiffviewStatusAdded", { guibg = "#282c34" })
--- vim.api.nvim_set_hl(0, "DiffviewStatusDeleted", { guibg = "#282c34" })
-vim.api.nvim_set_hl(0, "DiffChange", { bg = "#323a5a" })
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#273644" })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = "#273644" })
 vim.api.nvim_set_hl(0, "Folded", { bg = "#363c58", fg = "#7a88cf" })
