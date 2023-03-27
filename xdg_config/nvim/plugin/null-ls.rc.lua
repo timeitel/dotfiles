9e1df1f8154636c1460a4d288afb12f28923c115
Tim Eitel <timothy.eitel@news.com.au>
2023-03-27 18:48:01 +0800

    feat: lsp & TN

--- xdg_config/nvim/plugin/null-ls.rc.lua
+++ xdg_config/nvim/plugin/null-ls.rc.lua
@@ -3,12 +3,23 @@ if not ok then
   return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettierd,
   },
   on_attach = function(client, bufnr)
-    require("lsp-format-modifications").attach(client, bufnr, { format_on_save = true })
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
   end,
 })
