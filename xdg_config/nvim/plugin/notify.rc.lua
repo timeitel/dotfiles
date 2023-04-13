347f2a8c60bebc002e2807d92b33e05910f6630f
Tim Eitel <timeitel@outlook.com>
2023-04-13 22:18:23 +1000

    feat: remove notify until ts is fixed

--- xdg_config/nvim/plugin/notify.rc.lua
+++ xdg_config/nvim/plugin/notify.rc.lua
@@ -1,1 +1,2 @@
-vim.notify = require("notify") -- set plugins to use 'notify'
+-- TODO: uncomment when tree-sitter warnings stop spamming
+-- vim.notify = require("notify") -- set plugins to use 'notify'
