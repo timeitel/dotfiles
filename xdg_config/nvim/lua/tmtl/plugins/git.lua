34591565134c8167e8e23f45c80f9ff6ca9def14
Tim Eitel <timothy.eitel@news.com.au>
2023-04-14 23:22:01 +1000

    rf(plugin): neogit

--- xdg_config/nvim/lua/tmtl/plugins/git.lua
+++ xdg_config/nvim/lua/tmtl/plugins/git.lua
@@ -1,12 +1,31 @@
 local M = {
+  "bobrown101/git-blame.nvim",
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
     end,
   },
-  { "TimUntersberger/neogit", dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" } },
-  "bobrown101/git-blame.nvim",
+  {
+    "TimUntersberger/neogit",
+    config = function()
+      require("neogit").setup({
+        disable_commit_confirmation = true,
+        disable_insert_on_commit = false,
+        integrations = {
+          diffview = true,
+        },
+        mappings = {
+          status = {
+            ["d"] = function() -- open diff anywhere in status window and in an actual diffview tab, not a neogit wrapper
+              vim.cmd([[ DiffviewOpen ]])
+            end,
+          },
+        },
+      })
+    end,
+    dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+  },
 }
 
 return M
