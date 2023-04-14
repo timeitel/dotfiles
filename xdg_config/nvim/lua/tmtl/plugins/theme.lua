29b80c9acf36da1936e9b1ca1d470f379f94a2c4
Tim Eitel <timothy.eitel@news.com.au>
2023-04-14 22:28:53 +1000

    rf(plugin): colors

--- xdg_config/nvim/lua/tmtl/plugins/theme.lua
+++ xdg_config/nvim/lua/tmtl/plugins/theme.lua
@@ -3,9 +3,11 @@ local M = {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd([[colorscheme tokyonight-moon]])
-    end,
+    config = {
+      on_colors = function(colors)
+        colors.bg = "#282c34" -- gunmetal
+      end,
+    },
   },
 
   "nvim-lualine/lualine.nvim",
