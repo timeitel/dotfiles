02b5417c8873d8fc3f405c36dec8e1dc9d8e2976
Tim Eitel <timothy.eitel@news.com.au>
2023-04-15 15:01:44 +1000

    rf(plugin): ts indent

--- xdg_config/nvim/plugin/indent-object.rc.lua
+++
@@ -1,9 +0,0 @@
-local map = require("tmtl.utils").map
-
-map({ "x", "o" }, "ai", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>")
--- ensure selecting entire line (or just use Vai)
-map({ "x", "o" }, "aI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>")
--- select inner block (only if block, only else block, etc.)
-map({ "x", "o" }, "ii", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>")
--- select entire inner range (including if, else, etc.)
-map({ "x", "o" }, "iI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>")
