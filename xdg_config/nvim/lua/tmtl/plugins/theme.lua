0214eddf9449a46ebf70f14133f7d25dcdf0bf5f
Tim Eitel <timothy.eitel@news.com.au>
2023-04-15 16:19:14 +1000

    rf(plugin): tokyonight

--- xdg_config/nvim/lua/tmtl/plugins/theme.lua
+++ xdg_config/nvim/lua/tmtl/plugins/theme.lua
@@ -1,35 +1,38 @@
 local M = {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = {
-      on_colors = function(colors)
-        colors.bg = "#282c34" -- gunmetal
-      end,
-    },
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("tokyonight").setup({
+				on_colors = function(colors)
+					colors.bg = "#282c34" -- gunmetal
+				end,
+			})
+			vim.cmd([[colorscheme tokyonight-moon]])
+		end,
+	},
 
-  {
-    "akinsho/bufferline.nvim",
-    version = "v3.*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = {
-      options = {
-        mode = "tabs",
-        buffer_close_icon = "",
-        show_duplicate_prefix = false,
-        max_name_length = 30,
-        always_show_bufferline = false,
-      },
-    },
-  },
-  {
-    "nvim-zh/colorful-winsep.nvim",
-    config = function()
-      require("colorful-winsep").setup()
-    end,
-  },
+	{
+		"akinsho/bufferline.nvim",
+		version = "v3.*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = {
+			options = {
+				mode = "tabs",
+				buffer_close_icon = "",
+				show_duplicate_prefix = false,
+				max_name_length = 30,
+				always_show_bufferline = false,
+			},
+		},
+	},
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		config = function()
+			require("colorful-winsep").setup()
+		end,
+	},
 }
 
 return M
