4d5cf0e5cce8ba56862bde68b13d6e7f54f1b3cd
Tim Eitel <timothy.eitel@news.com.au>
2023-04-15 23:18:31 +1000

    fix: deprecated config

--- xdg_config/nvim/lua/tmtl/plugins/neoscroll.lua
+++ xdg_config/nvim/lua/tmtl/plugins/neoscroll.lua
@@ -1,12 +1,12 @@
 local M = {
-  "karb94/neoscroll.nvim",
-  config = {
-    mappings = { "<C-u>", "<C-d>" },
-    hide_cursor = false, -- Hide cursor while scrolling
-    stop_eof = false, -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  },
+	"karb94/neoscroll.nvim",
+	opts = {
+		mappings = { "<C-u>", "<C-d>" },
+		hide_cursor = false, -- Hide cursor while scrolling
+		stop_eof = false, -- Stop at <EOF> when scrolling downwards
+		respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+		cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+	},
 }
 
 return M
