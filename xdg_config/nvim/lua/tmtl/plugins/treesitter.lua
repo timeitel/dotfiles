0974f8001f2865ae8f20648f489341f0fb114660
Tim Eitel <timothy.eitel@news.com.au>
2023-04-15 12:04:54 +1000

    rf(plugin): treesitter

--- xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
+++ xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
@@ -1,5 +1,81 @@
 local M = {
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter.configs")
+
+      -- TODO: use diffrent indent plugin than ts
+      -- TODO: index ts not working in tsx
+      ts.setup({
+        highlight = {
+          enable = true,
+          disable = {},
+        },
+        indent = {
+          enable = true,
+          disable = {},
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+          },
+        },
+        ensure_installed = {
+          "tsx",
+          "typescript",
+          "javascript",
+          "python",
+          "rust",
+          "markdown",
+          "json",
+          "yaml",
+          "css",
+          "html",
+          "lua",
+        },
+        autotag = {
+          enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = false,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
+
+          move = { enable = false },
+          lsp_interop = {
+            enable = true,
+            border = "none",
+            peek_definition_code = {
+              ["<leader>pf"] = "@function.outer", -- preview function
+            },
+          },
+        },
+      })
+
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+    end,
+  },
   "nvim-treesitter/nvim-treesitter-context",
   "nvim-treesitter/nvim-treesitter-textobjects",
   "kiyoon/treesitter-indent-object.nvim",
