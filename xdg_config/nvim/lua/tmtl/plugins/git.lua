058c8900b944171aa6fe3731d37f6ff7b0996087
Tim Eitel <timothy.eitel@news.com.au>
2023-04-15 11:43:19 +1000

    rf(plugin): diffview

--- xdg_config/nvim/lua/tmtl/plugins/git.lua
+++ xdg_config/nvim/lua/tmtl/plugins/git.lua
@@ -1,6 +1,95 @@
 local M = {
   "bobrown101/git-blame.nvim",
   {
+    "sindrets/diffview.nvim",
+    config = function()
+      local actions = require("diffview.actions")
+
+      local shared_maps = {
+        ["L"] = false,
+        ["q"] = "<cmd>DiffviewClose<cr>",
+        ["s"] = function()
+          vim.cmd([[DiffviewClose]])
+          require("neogit").open()
+        end,
+        ["f"] = "<cmd>DiffviewToggleFiles<cr>",
+        ["<leader>f"] = "<cmd>DiffviewFocusFiles<cr>",
+        ["O"] = actions.focus_entry,
+        ["<C-e>"] = actions.scroll_view(1),
+        ["<C-y>"] = actions.scroll_view(-1),
+        ["<C-d>"] = actions.scroll_view(10),
+        ["<C-u>"] = actions.scroll_view(-10),
+        ["gf"] = actions.goto_file_edit,
+        ["x"] = function()
+          if vim.fn.confirm("", "Are you sure you'd like to discard changes? (&Yes\n&No)", 1) == 1 then
+            actions.restore_entry()
+            vim.cmd([[checktime]])
+          end
+        end,
+        ["<leader>sr"] = actions.unstage_all, -- stage reset
+        ["<leader>sf"] = actions.toggle_stage_entry,
+        ["<leader>sa"] = actions.stage_all,
+        -- Commit staged files
+        ["<leader>cs"] = function()
+          actions.focus_files()
+          Open_Git_Commit()
+        end,
+      }
+
+      -- Not using shallow copy util as mappings seem to leak into other diffview contexts
+      local merge_tool_maps = {
+        ["L"] = false,
+        ["q"] = "<cmd>DiffviewClose<cr>",
+        ["s"] = function()
+          vim.cmd([[DiffviewClose]])
+          require("neogit").open()
+        end,
+        ["f"] = "<cmd>DiffviewToggleFiles<cr>",
+        ["<leader>f"] = "<cmd>DiffviewFocusFiles<cr>",
+        ["O"] = actions.focus_entry,
+        ["<C-e>"] = actions.scroll_view(1),
+        ["<C-y>"] = actions.scroll_view(-1),
+        ["<C-d>"] = actions.scroll_view(10),
+        ["<C-u>"] = actions.scroll_view(-10),
+        ["gf"] = actions.goto_file_edit,
+        ["x"] = function()
+          if vim.fn.confirm("", "Are you sure you'd like to discard changes? (&Yes\n&No)", 1) == 1 then
+            actions.restore_entry()
+            vim.cmd([[checktime]])
+          end
+        end,
+        ["<leader>sr"] = actions.unstage_all, -- stage reset
+        ["<leader>sf"] = actions.toggle_stage_entry,
+        ["<leader>sa"] = actions.stage_all,
+        -- Commit staged files
+        ["<leader>cs"] = function()
+          actions.focus_files()
+          Open_Git_Commit()
+        end,
+      }
+      merge_tool_maps["<C-j>"] = actions.next_conflict
+      merge_tool_maps["<C-k>"] = actions.prev_conflict
+
+      require("diffview").setup({
+        view = {
+          merge_tool = {
+            layout = "diff3_mixed",
+          },
+        },
+        file_panel = {
+          listing_style = "list",
+        },
+        keymaps = {
+          view = shared_maps,
+          file_history_panel = shared_maps,
+          file_panel = shared_maps,
+          diff_view = shared_maps,
+          merge_tool = merge_tool_maps,
+        },
+      })
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
