2c67c1d7eb4fa01c1374c2a0679da63b80cd5bdc
Tim Eitel <timeitel@outlook.com>
2023-04-16 21:58:36 +1000

    feat: bg of qf list

--- xdg_config/nvim/after/ftplugin/qf.lua
+++ xdg_config/nvim/after/ftplugin/qf.lua
@@ -3,3 +3,11 @@ local map = require("tmtl.utils").map
 map("n", "q", function()
   vim.cmd([[cclose]])
 end, { desc = "Close quickfix list", buffer = true })
+
+local function color()
+  vim.defer_fn(function()
+    vim.cmd("set winhighlight=Normal:MyNormal,NormalNC:MyNormalNC")
+  end, 20)
+end
+
+color()
