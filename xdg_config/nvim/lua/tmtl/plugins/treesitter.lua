b0e7448d065696cad5b018d18d8e03ec96e7ab1e
Tim Eitel <timothy.eitel@news.com.au>
2023-04-17 21:29:35 +1000

    chore: lint project

--- xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
+++ xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
@@ -1,97 +1,97 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter-context",
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local ts = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter-context",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter.configs")
 
-			-- TODO: use diffrent indent plugin than ts
-			-- TODO: index ts not working in tsx
-			ts.setup({
-				highlight = {
-					enable = true,
-					disable = {},
-				},
-				indent = {
-					enable = true,
-					disable = {},
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<CR>",
-						scope_incremental = "<CR>",
-						node_incremental = "<TAB>",
-						node_decremental = "<S-TAB>",
-					},
-				},
-				ensure_installed = {
-					"tsx",
-					"typescript",
-					"javascript",
-					"python",
-					"rust",
-					"markdown",
-					"json",
-					"yaml",
-					"css",
-					"html",
-					"lua",
-				},
-				autotag = {
-					enable = true,
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = false,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-						include_surrounding_whitespace = true,
-					},
+      -- TODO: use diffrent indent plugin than ts
+      -- TODO: index ts not working in tsx
+      ts.setup({
+        highlight = {
+          enable = true,
+          disable = {},
+        },
+        indent = {
+          enable = true,
+          disable = {},
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+          },
+        },
+        ensure_installed = {
+          "tsx",
+          "typescript",
+          "javascript",
+          "python",
+          "rust",
+          "markdown",
+          "json",
+          "yaml",
+          "css",
+          "html",
+          "lua",
+        },
+        autotag = {
+          enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = false,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
 
-					move = { enable = false },
-					lsp_interop = {
-						enable = true,
-						border = "none",
-						peek_definition_code = {
-							["<leader>pf"] = "@function.outer", -- preview function
-						},
-					},
-				},
-			})
+          move = { enable = false },
+          lsp_interop = {
+            enable = true,
+            border = "none",
+            peek_definition_code = {
+              ["<leader>pf"] = "@function.outer", -- preview function
+            },
+          },
+        },
+      })
 
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-		end,
-	},
-	{
-		"kiyoon/treesitter-indent-object.nvim",
-		config = function()
-			local map = require("tmtl.utils").map
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+    end,
+  },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    config = function()
+      local map = require("tmtl.utils").map
 
-			map({ "x", "o" }, "ai", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>")
-			-- ensure selecting entire line (or just use Vai)
-			map({ "x", "o" }, "aI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>")
-			-- select inner block (only if block, only else block, etc.)
-			map({ "x", "o" }, "ii", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>")
-			-- select entire inner range (including if, else, etc.)
-			map({ "x", "o" }, "iI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>")
-		end,
-	},
+      map({ "x", "o" }, "ai", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>")
+      -- ensure selecting entire line (or just use Vai)
+      map({ "x", "o" }, "aI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>")
+      -- select inner block (only if block, only else block, etc.)
+      map({ "x", "o" }, "ii", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>")
+      -- select entire inner range (including if, else, etc.)
+      map({ "x", "o" }, "iI", "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>")
+    end,
+  },
 }
 
 return M
