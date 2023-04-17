9878d1b5b5087b55337d255e08a1903dbcc9cd5b
Tim Eitel <timothy.eitel@news.com.au>
2023-04-17 18:06:57 +1000

    feat(harpoon): maps

---
+++ xdg_config/nvim/after/ftplugin/harpoon.lua
@@ -0,0 +1,7 @@
+local map = require("tmtl.utils").map
+
+map("n", "o", function()
+	local pos = vim.api.nvim_win_get_cursor(0)
+	local row = pos[1]
+	require("harpoon.ui").nav_file(row)
+end, { desc = "Open filen", buffer = true })
