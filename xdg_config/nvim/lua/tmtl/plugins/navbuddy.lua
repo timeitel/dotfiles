e5dd3ca8482192e34e9b67d847182b333c1e4385
Tim Eitel <122245979+timothyeitel@users.noreply.github.com>
2023-06-29 17:17:22 +1000

    feat: bring back lsp_config tsserver for now

--- xdg_config/nvim/lua/tmtl/plugins/navbuddy.lua
+++ xdg_config/nvim/lua/tmtl/plugins/navbuddy.lua
@@ -1,69 +1,70 @@
-local M = {
-  "SmiteshP/nvim-navbuddy",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "SmiteshP/nvim-navic",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    local navbuddy = require("nvim-navbuddy")
-    local actions = require("nvim-navbuddy.actions")
+-- local M = {
+--   "SmiteshP/nvim-navbuddy",
+--   dependencies = {
+--     "neovim/nvim-lspconfig",
+--     "SmiteshP/nvim-navic",
+--     "MunifTanjim/nui.nvim",
+--   },
+--   config = function()
+--     local navbuddy = require("nvim-navbuddy")
+--     local actions = require("nvim-navbuddy.actions")
 
-    navbuddy.setup({
-      window = {
-        border = "rounded",
-        size = "50%",
-        position = "100%",
-        sections = {
-          left = {
-            size = "30%",
-          },
-          mid = {
-            size = "40%",
-          },
-          right = {},
-        },
-      },
-      mappings = {
-        ["<esc>"] = actions.close,
-        ["q"] = actions.close,
-        ["<C-q>"] = actions.close,
+--     navbuddy.setup({
+--       window = {
+--         border = "rounded",
+--         size = "50%",
+--         position = "100%",
+--         sections = {
+--           left = {
+--             size = "30%",
+--           },
+--           mid = {
+--             size = "40%",
+--           },
+--           right = {},
+--         },
+--       },
+--       mappings = {
+--         ["<esc>"] = actions.close,
+--         ["q"] = actions.close,
+--         ["<C-q>"] = actions.close,
 
-        ["j"] = actions.next_sibling,
-        ["k"] = actions.previous_sibling,
+--         ["j"] = actions.next_sibling,
+--         ["k"] = actions.previous_sibling,
 
-        ["h"] = actions.parent,
-        ["l"] = actions.children,
+--         ["h"] = actions.parent,
+--         ["l"] = actions.children,
 
-        ["v"] = actions.visual_name,
-        ["V"] = actions.visual_scope,
+--         ["v"] = actions.visual_name,
+--         ["V"] = actions.visual_scope,
 
-        ["y"] = actions.yank_name,
-        ["Y"] = actions.yank_scope,
+--         ["y"] = actions.yank_name,
+--         ["Y"] = actions.yank_scope,
 
-        ["i"] = actions.insert_name,
-        ["I"] = actions.insert_scope,
+--         ["i"] = actions.insert_name,
+--         ["I"] = actions.insert_scope,
 
-        ["a"] = actions.append_name,
-        ["A"] = actions.append_scope,
+--         ["a"] = actions.append_name,
+--         ["A"] = actions.append_scope,
 
-        ["r"] = actions.rename,
+--         ["r"] = actions.rename,
 
-        ["d"] = actions.delete,
+--         ["d"] = actions.delete,
 
-        ["f"] = actions.fold_create,
-        ["F"] = actions.fold_delete,
+--         ["f"] = actions.fold_create,
+--         ["F"] = actions.fold_delete,
 
-        ["c"] = actions.comment,
+--         ["c"] = actions.comment,
 
-        ["<enter>"] = actions.select,
-        ["o"] = actions.select,
-      },
-      lsp = {
-        auto_attach = true,
-      },
-    })
-  end,
-}
+--         ["<enter>"] = actions.select,
+--         ["o"] = actions.select,
+--       },
+--       lsp = {
+--         auto_attach = true,
+--       },
+--     })
+--   end,
+-- }
 
-return M
+-- TODO: not working
+return {}
