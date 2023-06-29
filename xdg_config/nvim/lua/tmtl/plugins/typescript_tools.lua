8bdf5e0ae904f43c4eaaad34cdbd6a71e0e810c7
Tim Eitel <122245979+timothyeitel@users.noreply.github.com>
2023-06-29 17:17:22 +1000

    feat: bring back lsp_config tsserver for now

--- xdg_config/nvim/lua/tmtl/plugins/typescript_tools.lua
+++ xdg_config/nvim/lua/tmtl/plugins/typescript_tools.lua
@@ -1,15 +1,28 @@
-local M = {
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-  config = function()
-    require("typescript-tools").setup {
-      on_attach = function(_, bufnr)
-        local lsp_maps = require("tmtl.keymaps-lsp")
-        lsp_maps.attach(bufnr)
-      end,
-    }
-  end,
-}
+-- TODO: try again when more developed
+-- local M = {
+--   "pmizio/typescript-tools.nvim",
+--   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+--   opts = {},
+--   config = function()
+--     require("typescript-tools").setup {
+--       on_attach = function(_, bufnr)
+--         local lsp_maps = require("tmtl.keymaps-lsp")
+--         lsp_maps.attach(bufnr)
+--       end,
+--       -- settings = {
+--       --   tsserver_file_preferences = {
+--       --     includeInlayParameterNameHints = "all",
+--       --     includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+--       --     includeInlayFunctionParameterTypeHints = true,
+--       --     includeInlayVariableTypeHints = true,
+--       --     includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+--       --     includeInlayPropertyDeclarationTypeHints = true,
+--       --     includeInlayFunctionLikeReturnTypeHints = true,
+--       --     includeInlayEnumMemberValueHints = true,
+--       --   },
+--       -- },
+--     }
+--   end,
+-- }
 
-return M
+return {}
