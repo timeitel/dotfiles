58813a501145118c7d09b88b5a81cc515259e993
Tim Eitel <122245979+timothyeitel@users.noreply.github.com>
2023-06-30 17:17:51 +1000

    feat: surround aliases

--- xdg_config/nvim/lua/tmtl/plugins/init.lua
+++ xdg_config/nvim/lua/tmtl/plugins/init.lua
@@ -40,7 +40,16 @@ local M = {
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({})
+      require("nvim-surround").setup({
+        aliases = {
+          ["b"] = ")",
+          ["c"] = "}",
+          ["r"] = "]",
+          ["q"] = '"',
+          ["l"] = '`',
+          ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+        },
+      })
     end,
   },
   {
@@ -60,15 +69,6 @@ local M = {
     config = function()
       vim.g.unception_open_buffer_in_new_tab = true
     end,
-  },
-  {
-    "nat-418/boole.nvim",
-    opts = {
-      mappings = {
-        increment = "<C-a>",
-        decrement = "<C-x>",
-      },
-    },
   },
 }
 
