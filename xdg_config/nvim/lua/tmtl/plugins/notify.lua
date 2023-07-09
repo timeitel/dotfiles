bc22dac3e112e2dff8418d6af9d383a3ec4c3e4f
Tim Eitel <timeitel@outlook.com>
2023-07-09 16:49:50 +1000

    feat: notify timeout

--- xdg_config/nvim/lua/tmtl/plugins/notify.lua
+++ xdg_config/nvim/lua/tmtl/plugins/notify.lua
@@ -5,6 +5,10 @@ local M = {
     vim.notify = function(msg, level, opts)
       return require('notify')(msg, level, opts)
     end
+
+    require('notify').setup({
+      timeout = 1000
+    })
   end,
   event = "VeryLazy",
 }
