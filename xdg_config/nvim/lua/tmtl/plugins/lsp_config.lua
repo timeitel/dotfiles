dfbcea864bfc8f8d813760d134b52725685502e4
Tim Eitel <122245979+timothyeitel@users.noreply.github.com>
2023-07-12 05:01:08 +1000

    feat: revert diagflow back for now

--- xdg_config/nvim/lua/tmtl/plugins/lsp_config.lua
+++ xdg_config/nvim/lua/tmtl/plugins/lsp_config.lua
@@ -98,11 +98,26 @@ local M = {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
+    vim.diagnostic.config({
+      virtual_text = {
+        prefix = "●",
+      },
+      update_in_insert = true,
+      float = {
+        source = "always", -- Or "if_many"
+      },
+    })
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
       border = border,
     })
     vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
       border = border,
+    })
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = { spacing = 4, prefix = "●" },
+      severity_sort = true,
     })
 
     protocol.CompletionItemKind = {
