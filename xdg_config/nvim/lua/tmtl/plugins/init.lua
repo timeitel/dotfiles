dd7d706fa601f635fe3f18da1657e2e4aa63099a
Tim Eitel <122245979+timothyeitel@users.noreply.github.com>
2023-07-12 05:01:08 +1000

    feat: revert diagflow back for now

--- xdg_config/nvim/lua/tmtl/plugins/init.lua
+++ xdg_config/nvim/lua/tmtl/plugins/init.lua
@@ -6,10 +6,6 @@ local M = {
   "inkarkat/vim-ReplaceWithRegister",
   "tpope/vim-commentary",
   {
-    'dgagn/diagflow.nvim',
-    opts = {}
-  },
-  {
     "sQVe/sort.nvim",
     config = function()
       local map = require("tmtl.utils").map
