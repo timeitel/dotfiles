a920bc724ec36dd6969b1f628333ba19c7d4ea3f
Tim Eitel <timeitel@outlook.com>
2023-07-15 16:58:12 +1000

    feat: utils for hl group under cusor

--- xdg_config/nvim/lua/tmtl/utils.lua
+++ xdg_config/nvim/lua/tmtl/utils.lua
@@ -63,4 +63,10 @@ M.contains = function(table, value)
   return false
 end
 
+M.table_length = function(table)
+  local count = 0
+  for _ in pairs(table) do count = count + 1 end
+  return count
+end
+
 return M
