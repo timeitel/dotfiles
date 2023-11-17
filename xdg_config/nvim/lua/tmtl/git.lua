f429ce4c50435192e7aa2c98a8c7c5466a412997
Tim Eitel <timothy.eitel@news.com.au>
2023-11-17 22:45:20 +1100

    feat: revert not closing diffview after commit

--- xdg_config/nvim/lua/tmtl/git.lua
+++ xdg_config/nvim/lua/tmtl/git.lua
@@ -1,15 +1,14 @@
 local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
 
--- TODO: trial this for a while
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "NeogitCommitComplete",
---   group = group,
---   callback = function()
---     vim.defer_fn(function()
---       vim.cmd([[DiffviewClose]])
---     end, 150)
---   end,
--- })
+vim.api.nvim_create_autocmd("User", {
+  pattern = "NeogitCommitComplete",
+  group = group,
+  callback = function()
+    vim.defer_fn(function()
+      vim.cmd([[DiffviewClose]])
+    end, 150)
+  end,
+})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "NeogitPopup", "NeogitCommitMessage" },
