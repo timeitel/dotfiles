a9736b9fad7f510c6af7e9e6586342449a0a0e5c
Tim Eitel <timothy.eitel@news.com.au>
2023-11-22 20:06:01 +1100

    fix: notify with multiple lsps

--- xdg_config/nvim/lua/tmtl/plugins/notify.lua
+++ xdg_config/nvim/lua/tmtl/plugins/notify.lua
@@ -3,6 +3,12 @@ local M = {
   version = "*",
   config = function()
     vim.notify = function(msg, level, opts)
+      -- https://github.com/neovim/nvim-lspconfig/issues/1931
+      for _, banned in ipairs({ "No information available" }) do
+        if msg == banned then
+          return
+        end
+      end
       return require("notify")(msg, level, opts)
     end
 
