abcb715958aae65ccc80550b3cf3f497b61b4175
Tim Eitel <timothy.eitel@news.com.au>
2024-03-28 18:42:03 +1100

    feat: ts parse mdx as jsx

--- xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
+++ xdg_config/nvim/lua/tmtl/plugins/treesitter.lua
@@ -85,6 +85,13 @@ local M = {
 
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
       parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+
+      vim.filetype.add({
+        extension = {
+          mdx = 'jsx'
+        }
+      })
+
     end,
   },
   {
