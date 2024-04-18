fa160573c45069ad4767546579f938222b580c26
Tim Eitel <timothy.eitel@news.com.au>
2024-04-18 17:57:35 +1000

    feat: move app launcher to macos shortcuts

--- home/.hammerspoon/tmtl/apps.lua
+++ home/.hammerspoon/tmtl/apps.lua
@@ -1,19 +1,10 @@
-local function open_app(name)
-  return function()
-    hs.application.launchOrFocus(name)
-    if name == "Finder" then
-      hs.appfinder.appFromName(name):activate()
-    end
-  end
-end
+-- moved to macos shortcuts since it's faster
 
-local modifiers = { "ctrl", "cmd" }
-hs.hotkey.bind(modifiers, "h", open_app("Brave Browser"))
-hs.hotkey.bind(modifiers, "j", open_app("YT Music"))
-hs.hotkey.bind(modifiers, "k", open_app("Obsidian"))
-hs.hotkey.bind(modifiers, "l", open_app("kitty"))
-hs.hotkey.bind(modifiers, "n", open_app("Slack"))
-hs.hotkey.bind(modifiers, ";", open_app("Messenger"))
-hs.hotkey.bind(modifiers, "y", open_app("TickTick"))
-hs.hotkey.bind(modifiers, "m", open_app("Postman"))
--- TODO: change cmd shift a to cmd-k to change tabs, and cmd shift HL for changing tabs
+-- hs.hotkey.bind(modifiers, "h", open_app("Brave Browser"))
+-- hs.hotkey.bind(modifiers, "j", open_app("YT Music"))
+-- hs.hotkey.bind(modifiers, "k", open_app("Obsidian"))
+-- hs.hotkey.bind(modifiers, "l", open_app("kitty"))
+-- hs.hotkey.bind(modifiers, "n", open_app("Slack"))
+-- hs.hotkey.bind(modifiers, ";", open_app("Messenger"))
+-- hs.hotkey.bind(modifiers, "y", open_app("TickTick"))
+-- hs.hotkey.bind(modifiers, "m", open_app("Postman"))
