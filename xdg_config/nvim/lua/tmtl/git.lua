1ebb96026c60f9d17c62d6c1680c21d9e8af6e45
Tim Eitel <timothy.eitel@news.com.au>
2024-04-22 10:43:32 +1000

    feat: remove closing neogit after commit for multiple commit flows

--- xdg_config/nvim/lua/tmtl/git.lua
+++ xdg_config/nvim/lua/tmtl/git.lua
@@ -1,15 +1,5 @@
 local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "NeogitCommitComplete",
-  group = group,
-  callback = function()
-    vim.defer_fn(function()
-      vim.cmd([[DiffviewClose]])
-    end, 150)
-  end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "NeogitPopup", "NeogitCommitMessage" },
   group = group,
