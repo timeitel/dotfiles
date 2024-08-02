a147d74d24bd661d26e50bf79d48f17e25b66670
Tim Eitel <timothy.eitel@gmail.com>
2024-08-02 22:09:48 +1000

    feat: formatting

--- xdg_config/nvim/lua/tmtl/plugins/formatting.lua
+++ xdg_config/nvim/lua/tmtl/plugins/formatting.lua
@@ -2,14 +2,16 @@ local M = {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      lua = { "stylua" },
-      rust = { "rustfmt" },
-      go = { "goimports", "gofmt" },
-      templ = { "templ", "rustywind" },
       javascript = { "prettierd", "rustywind" },
       javascriptreact = { "prettierd", "rustywind" },
       typescript = { "prettierd", "rustywind" },
       typescriptreact = { "prettierd", "rustywind" },
+
+      lua = { "stylua" },
+      rust = { "rustfmt" },
+      go = { "goimports", "gofmt" },
+      html = { "djlint", "rustywind" },
+      yaml = { "yamlfix" },
     },
 
     format_on_save = {
