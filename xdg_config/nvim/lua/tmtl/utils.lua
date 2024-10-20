2bb54dc42b3cc59504b9feaf4bb3277c21fed502
Tim Eitel <timothy.eitel@gmail.com>
2024-10-20 17:08:53 +1100

    feat: notify command util

--- xdg_config/nvim/lua/tmtl/utils.lua
+++ xdg_config/nvim/lua/tmtl/utils.lua
@@ -65,28 +65,75 @@ end
 
 M.table_length = function(table)
   local count = 0
-  for _ in pairs(table) do count = count + 1 end
+  for _ in pairs(table) do
+    count = count + 1
+  end
   return count
 end
 
 M.change_case = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local word = vim.fn.expand('<cword>')
-  local word_start = vim.fn.matchstrpos(vim.fn.getline('.'), '\\k*\\%' .. (col + 1) .. 'c\\k*')[2]
+  local word = vim.fn.expand("<cword>")
+  local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
 
   -- Detect camelCase
-  if word:find('[a-z][A-Z]') then
+  if word:find("[a-z][A-Z]") then
     -- Convert camelCase to snake_case
-    local snake_case_word = word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
+    local snake_case_word = word:gsub("([a-z])([A-Z])", "%1_%2"):lower()
     vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
     -- Detect snake_case
-  elseif word:find('_[a-z]') then
+  elseif word:find("_[a-z]") then
     -- Convert snake_case to camelCase
-    local camel_case_word = word:gsub('(_)([a-z])', function(_, l) return l:upper() end)
+    local camel_case_word = word:gsub("(_)([a-z])", function(_, l)
+      return l:upper()
+    end)
     vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { camel_case_word })
   else
     print("Not a snake_case or camelCase word")
   end
+end
+
+M.notify_command = function(command)
+  local stdin = vim.loop.new_pipe()
+  local stdout = vim.loop.new_pipe()
+  local stderr = vim.loop.new_pipe()
+  local output = ""
+  local error_output = ""
+  local notify = function(msg, level)
+    vim.notify(msg, level, { title = table.concat(command, " ") })
+  end
+  vim.loop.spawn(command[1], {
+    stdio = { stdin, stdout, stderr },
+    detached = true,
+    args = #command > 1 and vim.list_slice(command, 2, #command) or nil,
+  }, function(code, _)
+    stdin:close()
+    stdout:close()
+    stderr:close()
+    if #output > 0 then
+      notify(output)
+    end
+    if #error_output > 0 then
+      notify(error_output, "error")
+    end
+    if #output == 0 and #error_output == 0 then
+      notify("No output of command, exit code: " .. code, "warn")
+    end
+  end)
+  stdout:read_start(function(err, data)
+    if err then
+      error_output = error_output .. (err or data)
+      return
+    end
+    if data then
+      output = output .. data
+    end
+  end)
+  stderr:read_start(function(err, data)
+    if err or data then
+      error_output = error_output .. (err or data)
+    end
+  end)
 end
 
 return M
