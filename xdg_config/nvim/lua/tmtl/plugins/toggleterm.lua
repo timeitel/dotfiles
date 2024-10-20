3f51aa7add0ffc851d787e35834ab77dc4c8adde
Tim Eitel <timothy.eitel@gmail.com>
2024-10-20 17:39:56 +1100

    feat: notify command to push

--- xdg_config/nvim/lua/tmtl/plugins/toggleterm.lua
+++ xdg_config/nvim/lua/tmtl/plugins/toggleterm.lua
@@ -17,17 +17,7 @@ local M = {
     end, { desc = "[R]un [W]atcher" })
 
     map("n", "<leader>gp", function()
-      local unpushed_commits = vim.fn.systemlist("git cherry")
-      local notify = require("notify")
-      local commits = table.getn(unpushed_commits)
-
-      if commits == 0 then
-        return notify("No commits to push")
-      end
-
-      notify("Pushing commits: " .. commits)
-      -- TODO: sometimes not submiting - command left in terminal without <CR>
-      vim.cmd([[ 10TermExec cmd="git push" name="Git Push" open=0 ]])
+      require("tmtl.utils").notify_command({ "git", "push" })
       vim.cmd([[doautocmd user FugitiveChanged]])
     end, { desc = "[G]it [P]ush" })
   end,
