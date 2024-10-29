f5cc0b44870bdc66816c6a8188fdd007808f1621
Tim Eitel <timothy.eitel@gmail.com>
2024-10-29 20:37:20 +1100

    rf: move scroll maps into plugin

--- xdg_config/nvim/lua/tmtl/plugins/neoscroll.lua
+++ xdg_config/nvim/lua/tmtl/plugins/neoscroll.lua
@@ -1,12 +1,36 @@
 local M = {
   "karb94/neoscroll.nvim",
-  opts = {
-    mappings = { "<C-u>", "<C-d>" },
-    hide_cursor = false, -- Hide cursor while scrolling
-    stop_eof = false, -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  },
+  config = function()
+    local neoscroll = require("neoscroll")
+
+    neoscroll.setup({
+      mappings = { "<C-u>", "<C-d>" },
+      hide_cursor = false, -- Hide cursor while scrolling
+    })
+
+    local keymap = {
+      ["<C-u>"] = function()
+        neoscroll.ctrl_u({ duration = 50 })
+      end,
+      ["<C-d>"] = function()
+        neoscroll.ctrl_d({ duration = 50 })
+      end,
+      ["zt"] = function()
+        neoscroll.zt({ half_win_duration = 50 })
+      end,
+      ["zz"] = function()
+        neoscroll.zz({ half_win_duration = 50 })
+      end,
+      ["zb"] = function()
+        neoscroll.zb({ half_win_duration = 50 })
+      end,
+    }
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
+  end,
+  event = "BufRead",
 }
 
 return M
