38283765f99d4792173c45ad61823b4fed45b672
Tim Eitel <timothy.eitel@gmail.com>
2024-10-30 22:59:13 +1100

    fix: toggleterm keys

--- xdg_config/nvim/lua/tmtl/plugins/toggleterm.lua
+++ xdg_config/nvim/lua/tmtl/plugins/toggleterm.lua
@@ -1,12 +1,5 @@
 local M = {
   "akinsho/toggleterm.nvim",
-  keys = {
-    {
-      "<leader>rw",
-      "<CMD>TermExec cmd='docker compose up --watch' name='Docker Watcher'<CR>",
-      desc = "[R]un [W]atcher",
-    },
-  },
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<C-;>]],
@@ -16,6 +9,13 @@ local M = {
       },
       autochdir = true,
     })
+
+    vim.keymap.set(
+      "n",
+      "<leader>rw",
+      "<CMD>TermExec cmd='docker compose up --watch' name='Docker Watcher'<CR>",
+      { desc = "[R]un [W]atcher" }
+    )
   end,
 }
 
