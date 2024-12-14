f58202de1791b97fd903a40dfe55c9b181045918
Tim Eitel <timothy.eitel@gmail.com>
2024-12-14 13:49:38 +1100

    feat: close diffview on commit

--- xdg_config/nvim/after/ftplugin/gitcommit.lua
+++ xdg_config/nvim/after/ftplugin/gitcommit.lua
@@ -1,3 +1,12 @@
 vim.fn.feedkeys("i")
 
-vim.keymap.set("n", "q", "<CMD>close<CR>", { desc = "Close status buffer", buffer = true })
+vim.keymap.set("n", "q", "<CMD>close!<CR>", { desc = "Close status buffer", buffer = true })
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  buffer = 0,
+  callback = function()
+    vim.schedule(function()
+      vim.cmd("DiffviewClose")
+    end)
+  end,
+})
