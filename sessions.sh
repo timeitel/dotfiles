b37bd06b22885b247fd1e8f3736ea47c38e8075d
Tim Eitel <timothy.eitel@gmail.com>
2025-01-03 20:10:05 +1100

    feat: tmux theme and tpm

--- sessions.sh
+++ sessions.sh
@@ -4,7 +4,7 @@ tmux has-session -t $SESH 2>/dev/null
 
 if [ $? != 0 ]; then
   tmux new-session -d -s $SESH -n "edit"
-  tmux send-keys -t $SESH:editor "cd ~" C-m
+  tmux send-keys -t $SESH:edit "vc" C-m
 
   tmux new-window -t $SESH -n "ps" -d
   tmux send-keys -t $SESH:ps "ls" C-m
