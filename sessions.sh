d5d0c6e810dcf0fb1f2ca4ce923458d560e66a56
Tim Eitel <timothy.eitel@gmail.com>
2025-01-03 19:41:43 +1100

    chore: tmux session script

---
+++ sessions.sh
@@ -0,0 +1,13 @@
+SESH="w"
+
+tmux has-session -t $SESH 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -d -s $SESH -n "edit"
+  tmux send-keys -t $SESH:editor "cd ~" C-m
+
+  tmux new-window -t $SESH -n "ps" -d
+  tmux send-keys -t $SESH:ps "ls" C-m
+fi
+
+tmux attach-session -t $SESH
