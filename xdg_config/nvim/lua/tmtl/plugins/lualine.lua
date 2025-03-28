5517029f8c20a2b9486209ae18c19afade939e0f
Tim Eitel <timothy.eitel@gmail.com>
2025-03-28 22:52:08 +1100

    feat: deprecation warning from 0.11 bump

--- xdg_config/nvim/lua/tmtl/plugins/lualine.lua
+++ xdg_config/nvim/lua/tmtl/plugins/lualine.lua
@@ -94,12 +94,13 @@ local M = {
     local lsp = {
       function()
         local msg = "No LSP"
-        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+        local buf_ft = vim.api.nvim_get_option_value("filetype", { scope = "local" })
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
           return msg
         end
         for _, client in ipairs(clients) do
+          ---@diagnostic disable-next-line: undefined-field
           local filetypes = client.config.filetypes
           if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
             return " " .. client.name
