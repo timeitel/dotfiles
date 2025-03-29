38e501972af865813a20363a3616a06e0c4b029f
Tim Eitel <timothy.eitel@gmail.com>
2025-03-29 11:16:59 +1100

    fix: telescope winborder conflict with new option

--- xdg_config/nvim/lua/tmtl/autocmds.lua
+++ xdg_config/nvim/lua/tmtl/autocmds.lua
@@ -74,3 +74,16 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   end,
   group = group,
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TelescopeFindPre",
+  callback = function()
+    vim.opt_local.winborder = "none"
+    vim.api.nvim_create_autocmd("WinLeave", {
+      once = true,
+      callback = function()
+        vim.opt_local.winborder = "rounded"
+      end,
+    })
+  end,
+})
